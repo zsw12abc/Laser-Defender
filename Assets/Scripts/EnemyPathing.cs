088e14eda70af9a308c1b903b5e1e405f94ce22a
Shaowei <Shaowei.Zhang@Outlook.com>
2019-02-01 23:17:55 +1100

    Move Enemy On Path

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -6,13 +6,33 @@ public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] private List<Transform> wayPoints;
 
+    [SerializeField] private float moveSpeed = 2f;
+
     // Start is called before the first frame update
-    void Start()
+    private int _wayPointIndex = 0;
+
+    private void Start()
     {
+        transform.position = wayPoints[_wayPointIndex].transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (_wayPointIndex <= wayPoints.Count - 1)
+        {
+            var targetPosition = wayPoints[_wayPointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            if (transform.position == targetPosition) _wayPointIndex++;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
