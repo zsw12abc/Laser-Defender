2bf805dd8d8f58d6f49c13bab069767211bc1c9b
Shaowei <Shaowei.Zhang@Outlook.com>
2019-02-02 17:14:06 +1100

    scrolling background

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] private float backgroundScrollSpeed = 0.5f;
+
+    private Material _material;
+    private Vector2 _offSet;
+
+    private void Start()
+    {
+        _material = GetComponent<Renderer>().material;
+        _offSet = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    private void Update()
+    {
+        _material.mainTextureOffset += _offSet * Time.deltaTime;
+    }
+}
